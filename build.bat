fae1ca86b5b530258c3a723b72a076ff4398c6e4
Glenn Fiedler <glenn@networknext.com>
2024-02-15 10:50:22 -0500

    once again

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 echo hello batch file world
 premake5 vs2019
-"c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" visualstudio/next.sln /Build $1 
+"c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" visualstudio/next.sln /Build $CONFIG
