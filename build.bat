cc008c083cd153449a510d5d13382a7fcf5630cd
Glenn Fiedler <glenn@networknext.com>
2024-02-15 10:41:25 -0500

    again

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 echo hello batch file world
-premake5 vs2022
+premake5 vs2019
 "c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" /Run visualstudio/next.sln /Project test
