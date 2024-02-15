d90bcc2c1ca037df4243dde4995983252a14b33a
Glenn Fiedler <glenn@networknext.com>
2024-02-15 10:59:55 -0500

    like an idiot, i keep trying to make it work

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
+echo $CONFIG
 premake5 vs2019
-"c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" visualstudio/next.sln /Build $1
+"c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" visualstudio/next.sln /Build $CONFIG
