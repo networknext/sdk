58e9abc754efd3595e9181212df37d52d334a081
Glenn Fiedler <glenn@networknext.com>
2024-02-15 11:04:09 -0500

    i hate windows

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
-echo $CONFIG
+echo $1
 premake5 vs2019
-"c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" visualstudio/next.sln /Build $CONFIG
+"c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" visualstudio/next.sln /Build $1
